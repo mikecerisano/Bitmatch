c7eb7f1f12272048a81b5b0a06eccf5c81cdf3d8
Michael Cerisano <mjcerisano@gmail.com>
2025-08-17 22:30:37 -0400

    Initial Commit

---
+++ BitMatchTests/BitMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitMatchTests.swift
+//  BitMatchTests
+//
+//  Created by Mike Cerisano on 8/17/25.
+//
+
+import Testing
+@testable import BitMatch
+
+struct BitMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
